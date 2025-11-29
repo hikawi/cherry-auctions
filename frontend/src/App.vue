52a69b656f9515019e87b100f219a532ae3f8ab1
Nguyễn Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2025-11-29 12:16:29 +0700

    chore: setup vuejs frontend with docker
    Set up a VueJS, TypeScript, Pinia and Vue Router.
    Set up Docker Compose with hot reload for frontend.
    
    Closes #8

---
+++ frontend/src/App.vue
@@ -0,0 +1,11 @@
+<script setup lang="ts"></script>
+
+<template>
+  <h1>You did it!</h1>
+  <p class="text-cyan-600 font-bold">
+    Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
+    documentation
+  </p>
+</template>
+
+<style scoped></style>
