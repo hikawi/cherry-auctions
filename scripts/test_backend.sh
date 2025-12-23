db5c445e2ba1b7fccd78760b671bdb9cf9ffb25e
Nguyễn Nguyệt Ánh <128211112+hikawi@users.noreply.github.com>
2025-12-24 06:18:15 +0700

    test: add missing tests for s3 integration

---
+++ scripts/test_backend.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+SMTP_HOST=localhost \
+  SMTP_PORT=1025 \
+  SMTP_USER=test@example.com \
+  SMTP_PASSWORD=test \
+  AWS_ACCESS_KEY_ID=82d0d2b9a0cc2ba07bcb \
+  AWS_SECRET_ACCESS_KEY=pX8uRMhyG26ENPoTUSZY9ziwxba0VrQKdtefOqcA \
+  AWS_S3_BASE=http://localhost:9000 \
+  AWS_S3_USE_PATH_STYLE=true \
+  AWS_BUCKET_NAME=cherry-auctions \
+  GIN_MODE=release \
+  go test -v ./test
