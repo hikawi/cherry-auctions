c55c80887fa251bc39d2bbfa568bb2111bc70f9b
tsukimi <128211112+hikawi@users.noreply.github.com>
2025-12-23 15:59:37 +0700

    chore: setup local rustfs for dev environment

--- backend/routes/test/handler.go
+++ backend/routes/test/handler.go
@@ -5,12 +5,14 @@ import (
 
 	"github.com/aws/aws-sdk-go-v2/service/s3"
 	"github.com/gin-gonic/gin"
+	"luny.dev/cherryauctions/utils"
 )
 
 func (h *TestHandler) GetTest(g *gin.Context) {
 	ctx := context.Background()
 	res, err := h.S3Client.ListBuckets(ctx, &s3.ListBucketsInput{})
 	if err != nil {
+		utils.Log(gin.H{"error": err})
 		g.AbortWithStatusJSON(500, gin.H{"error": "internal server error"})
 		return
 	}
